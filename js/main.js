f4cd58d32c0906c2381549b8c7866d899f5df95a
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2026-05-15 11:26:05 +0200

    feat: implement player movement

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
-console.log("hello world");
+
+class Player {
+    constructor(){
+        this.positionX = 50
+        this.positionY = 0
+        this.width = 20
+        this.height = 10
+
+        this.updateUI()
+    }
+    updateUI(){
+        const playerElm = document.getElementById("player")
+        playerElm.style.left = this.positionX + "vw"
+        playerElm.style.bottom = this.positionY + "vh"
+        playerElm.style.width = this.width + "vw"
+        playerElm.style.height = this.height + "vh"
+    }
+    moveLeft(){
+        if (this.positionX > 0) {
+            this.positionX--
+            this.updateUI()
+        }
+    }
+    moveRight(){
+        if(this.positionX < 100 - this.width){
+            this.positionX++
+            this.updateUI()
+        }
+    }
+}
+
+
+const player = new Player()
+
+document.addEventListener("keydown", (e) => {
+    if(e.code === "ArrowLeft") {
+        player.moveLeft()
+    } else if (e.code === "ArrowRight") {
+        player.moveRight()
+    }
+})
